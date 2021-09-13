754713d6e14401102ae684e6350489fe83ce6fda
Chris Chamberlain <chamberlain@live.ca>
2021-09-12 21:56:41 -0600

    Initial commit, example project

---
+++ Assets/Scripts/TossDiceBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TossDiceBehaviour : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var x = Random.Range(-2f, +2f);
+            var z = Random.Range(-2f, +2f);
+            rigidbody.position = new Vector3(x, 2, z);
+
+            rigidbody.angularVelocity = Random.onUnitSphere * 45;
+            rigidbody.velocity = Random.onUnitSphere * 5;
+        }
+    }
+}
